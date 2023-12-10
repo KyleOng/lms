217fed2512df95a8a7210d06b2f17f4f8b56fea3
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2023-12-10 22:53:27 +0800

    feat: add teacher page with dynamic layout

---
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const AnalyticsPage = (props: Props) => {
+  return (
+    <div>Analytics Page!</div>
+  )
+}
+
+export default AnalyticsPage
---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const CoursesPage = (props: Props) => {
+  return (
+    <div>This is a course page!</div>
+  )
+}
+
+export default CoursesPage
