8c796cf8a6a0dcbdb2515337d8bb144fbad9181c
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2023-12-10 22:52:43 +0800

    fix: hooks need use client

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 type Props = {};
 
 const NavbarRoutes = (props: Props) => {
+  const pathname = usePathname();
+
   return (
     <div className="flex gap-x-2 ml-auto">
       <UserButton />
