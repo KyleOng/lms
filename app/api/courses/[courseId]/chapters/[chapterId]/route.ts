7585788df33fd320a491bf023b1b2550458e416f
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2023-12-20 17:14:34 +0800

    feat: create api route for chapter deletion

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -8,6 +8,66 @@ const { Video } = new Mux(
   process.env.MUX_TOKEN_SECRET!
 );
 
+export async function DELETE(
+  req: Request,
+  {
+    params: { courseId, chapterId },
+  }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) return new Response("Unauthorized", { status: 401 });
+
+    const courseOwner = await db.course.findUnique({
+      where: { id: courseId, userId },
+    });
+
+    if (!courseOwner) return new Response("Unauthorized", { status: 401 });
+
+    const chapter = await db.chapter.findUnique({
+      where: { id: chapterId, courseId },
+    });
+
+    if (!chapter) return new Response("Not Found", { status: 404 });
+
+    if (chapter.videoUrl) {
+      const exisitingMuxData = await db.muxData.findFirst({
+        where: { chapterId },
+      });
+
+      if (exisitingMuxData) {
+        await Video.Assets.del(exisitingMuxData.assetId);
+        await db.muxData.delete({ where: { id: exisitingMuxData.id } });
+      }
+    }
+
+    const deletedChapter = await db.chapter.delete({
+      where: { id: chapterId, courseId },
+    });
+
+    const publishedChaptersInCourse = await db.chapter.findMany({
+      where: { courseId, isPublished: true },
+    });
+
+    if (!publishedChaptersInCourse.length) {
+      await db.course.update({
+        where: {
+          id: courseId,
+        },
+        data: {
+          isPublished: false,
+        },
+      });
+    }
+
+    return NextResponse.json(deletedChapter);
+  } catch (error) {
+    console.log("[COURSE_CHAPTER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   {
@@ -45,7 +105,7 @@ export async function PATCH(
       const asset = await Video.Assets.create({
         input: values.videoUrl,
         playback_policy: "public",
-        test: false
+        test: false,
       });
 
       await db.muxData.create({
@@ -53,8 +113,8 @@ export async function PATCH(
           chapterId,
           assetId: asset.id,
           playbackId: asset.playback_ids?.[0]?.id,
-        }
-      })
+        },
+      });
     }
 
     return NextResponse.json(chapter);
