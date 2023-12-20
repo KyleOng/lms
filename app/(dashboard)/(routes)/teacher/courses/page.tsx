834a3c61ce087cb5242941facd28411dbd2c0c8c
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2023-12-20 20:57:24 +0800

    fix: add data table and search query in Course page

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,19 +1,31 @@
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import React from 'react'
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { db } from "@/lib/db";
 
-type Props = {}
+type Props = {};
 
-const CoursesPage = (props: Props) => {
+
+const CoursesPage = async (props: Props) => {
+  const {userId} = auth();
+  if (!userId) {
+    return redirect("/")
+  }
+  const courses = await db.course.findMany({
+    where: {
+      userId
+    },
+    orderBy: {
+      createdAt: "desc"
+    }
+  })
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>
-          New Course
-        </Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
-  )
-}
+  );
+};
 
-export default CoursesPage
+export default CoursesPage;
