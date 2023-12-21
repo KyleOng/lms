0993bd21ef10706656ba38f681249764ed2f9d2e
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2023-12-21 22:16:31 +0800

    feat: add Chapter Id page

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -0,0 +1,64 @@
+import { getChapter } from "@/actions/get-chapter";
+import Banner from "@/components/banner";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
+import VideoPlayer from "./_components/VideoPlayer";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const { userId } = auth();
+  if (!userId) return redirect("/");
+
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    courseId: params.courseId,
+    chapterId: params.chapterId,
+    userId,
+  });
+
+  if (!chapter || !course) return redirect("/");
+
+  const isLocked = !chapter.isFree && !purchase;
+
+  const completedOnEnd = !!purchase && !userProgress?.isCompleted;
+
+  return (
+    <div>
+      {userProgress?.isCompleted && (
+        <Banner label="You already completed this chapter." variant="success" />
+      )}
+      {isLocked && (
+        <Banner
+          label="You need to purchase this course to watch this chapter"
+          variant="warning"
+        />
+      )}
+      <div className="flex flex-col max-w-4xl mx-auto pb-20">
+        <div className="p-4">
+          <VideoPlayer
+            chapterId={params.chapterId}
+            title={chapter.title}
+            courseId={params.courseId}
+            nextChapterId={nextChapter?.id}
+            playbackId={muxData?.playbackId!}
+            isLocked={isLocked}
+            completedOnEnd={completedOnEnd}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChapterIdPage;
