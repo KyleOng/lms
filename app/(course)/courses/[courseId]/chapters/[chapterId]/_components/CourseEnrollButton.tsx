da746dea63d40ab96e04745fb4b0f0b557ed2af8
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2023-12-22 21:36:24 +0800

    fix: on click course enroll button to call course enroll api

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollButton.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollButton.tsx
@@ -2,6 +2,9 @@
 
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface Props {
   price: number;
@@ -9,7 +12,28 @@ interface Props {
 }
 
 const CourseEnrollButton = ({ price, courseId }: Props) => {
-  return <Button size="sm" className="w-full md:w-auto">Enroll for {formatPrice(price)}</Button>;
+  const [isLoading, setIsLoading] = useState(false);
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
+      window.location.assign(response.data.url);
+    } catch {
+      toast.error("Something went wrong, please try again later");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <Button
+      onClick={onClick}
+      disabled={isLoading}
+      size="sm"
+      className="w-full md:w-auto"
+    >
+      Enroll for {formatPrice(price)}
+    </Button>
+  );
 };
 
 export default CourseEnrollButton;
