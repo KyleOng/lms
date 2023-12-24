7994ea1d83bbcf59a3ebf73194c1db88b318ce00
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2023-12-24 11:47:45 +0800

    fix: add course progress in course sidebar

--- app/(course)/courses/[courseId]/_components/CourseSidebar.tsx
+++ app/(course)/courses/[courseId]/_components/CourseSidebar.tsx
@@ -4,6 +4,7 @@ import { Chapter, Course, UserProgress } from "@prisma/client";
 import { redirect } from "next/navigation";
 import React from "react";
 import CourseSidebarItem from "./CourseSidebarItem";
+import CourseProgress from "@/components/course-progress";
 
 type Props = {
   course: Course & {
@@ -32,12 +33,16 @@ const CourseSidebar = async ({ course, progressCount }: Props) => {
     <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
       <div className="p-8 flex flex-col border-b">
         <h1 className="font-semibold">{course.title}</h1>
+        {purchase && (
+          <div className="mt-10">
+            <CourseProgress variant="success" value={progressCount} />
+          </div>
+        )}
       </div>
-      {/* Check purchase and add progress */}
       <div className="flex flex-col w-full">
         {course.chapters.map((chapter) => (
-          <CourseSidebarItem 
-            key={chapter.id} 
+          <CourseSidebarItem
+            key={chapter.id}
             id={chapter.id}
             label={chapter.title}
             isCompleted={chapter.userProgress?.[0]?.isCompleted!}
